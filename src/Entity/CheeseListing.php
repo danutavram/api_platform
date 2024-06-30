081405fb9d1b49997456fd590382286d7965135e
danutavram <avramnicolaedan@gmail.com>
2024-06-30 13:25:06 +0300

    Embedded & Collections

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -2,34 +2,72 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\BooleanFilter;
+use ApiPlatform\Doctrine\Orm\Filter\RangeFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Serializer\Filter\PropertyFilter;
 use App\Repository\CheeseListingRepository;
+use Carbon\Carbon;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\SerializedName;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CheeseListingRepository::class)]
-#[ApiResource]
+#[ApiResource(normalizationContext:["groups" => "cheese_listing:read", "swagger_definition_name" => "Read"], 
+            denormalizationContext:["groups" => "cheese_listing:write", "swagger_definition_name" => "Write"],
+            operations: [
+                new Get(normalizationContext: ['groups' => ['cheese_listing:read', 'cheese_listing:item:get']]),
+            ],
+            paginationItemsPerPage: 5)]
+#[ApiFilter(BooleanFilter::class, properties:["isPublished"])]
+#[ApiFilter(SearchFilter::class, properties:["title" => "partial", "description" => "partial"])]
+#[ApiFilter(RangeFilter::class, properties:["price"])]
+#[ApiFilter(PropertyFilter::class)]
 class CheeseListing
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
-
+    
     #[ORM\Column(length: 255)]
+    #[Groups(["cheese_listing:read", "cheese_listing:write", "user:read", "user:write"])]
+    #[Assert\NotBlank()]
+    #[Assert\Length(min:2, max:50, maxMessage:"Describe your cheese in 50 chars or less")]
     private ?string $title = null;
-
+    
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(["cheese_listing:read"])]
+    #[Assert\NotBlank()]
     private ?string $description = null;
-
+    
     #[ORM\Column]
+    #[Groups(["cheese_listing:read", "cheese_listing:write", "user:read", "user:write"])]
+    #[Assert\NotBlank()]
     private ?int $price = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $createdAt = null;
 
     #[ORM\Column]
-    private ?bool $isPublished = null;
+    private ?bool $isPublished = false;
+
+    #[ORM\ManyToOne(inversedBy: 'cheeseListings')]
+    #[ORM\JoinColumn(nullable: false)]
+    #[Groups(["cheese_listing:read", "cheese_listing:write"])]
+    #[Assert\Valid()]
+    private ?User $owner = null;
+
+    public function __construct(string $title = null)
+    {
+        $this->createdAt = new \DateTimeImmutable();
+        $this->title = $title;
+    }
 
     public function getId(): ?int
     {
@@ -41,21 +79,34 @@ class CheeseListing
         return $this->title;
     }
 
-    public function setTitle(string $title): static
-    {
-        $this->title = $title;
-
-        return $this;
-    }
-
     public function getDescription(): ?string
     {
         return $this->description;
     }
 
+    
+    #[Groups(["cheese_listing:read"])]
+    public function getShortDescription(): ?string
+    {
+        if (strlen($this->description) < 40) {
+            return $this->description;
+        }
+
+        return substr($this->description, 0, 40).'...';
+    }
+    
     public function setDescription(string $description): static
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    #[Groups(["cheese_listing:write", "user:write"])]
+    #[SerializedName("description")]
+    public function setTextDescription(string $description): static
+    {
+        $this->description = nl2br($description);
 
         return $this;
     }
@@ -77,11 +128,10 @@ class CheeseListing
         return $this->createdAt;
     }
 
-    public function setCreatedAt(\DateTimeInterface $createdAt): static
+    #[Groups(["cheese_listing:read"])]
+    public function getCreatedAtAgo(): string
     {
-        $this->createdAt = $createdAt;
-
-        return $this;
+        return Carbon::instance($this->getCreatedAt())->diffForHumans();
     }
 
     public function isPublished(): ?bool
@@ -92,6 +142,18 @@ class CheeseListing
     public function setPublished(bool $isPublished): static
     {
         $this->isPublished = $isPublished;
+
+        return $this;
+    }
+
+    public function getOwner(): ?User
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(?User $owner): static
+    {
+        $this->owner = $owner;
 
         return $this;
     }
---
+++ src/Entity/User.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[ApiResource(normalizationContext:["groups" => "user:read"], 
+denormalizationContext:["groups" => "user:write"],
+)]
+#[UniqueEntity(fields:["username"])]
+#[UniqueEntity(fields:["email"])]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180)]
+    #[Groups(["user:read", "user:write"])]
+    #[Assert\NotBlank()]
+    #[Assert\Email()]
+    private ?string $email = null;
+
+    /**
+     * @var list<string> The user roles
+     */
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    #[Groups(["user:write"])]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 255, unique:true)]
+    #[Groups(["user:read", "user:write", "cheese_listing:item:get", "cheese_listing:write"])]
+    #[Assert\NotBlank()]
+    private ?string $username = null;
+
+    /**
+     * @var Collection<int, CheeseListing>
+     */
+    #[ORM\OneToMany(targetEntity: CheeseListing::class, mappedBy: 'owner', cascade:['persist'], orphanRemoval:true)]
+    #[Groups(["user:read", "user:write"])]
+    #[Assert\Valid()]
+    private Collection $cheeseListings;
+
+    public function __construct()
+    {
+        $this->cheeseListings = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     *
+     * @return list<string>
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, CheeseListing>
+     */
+    public function getCheeseListings(): Collection
+    {
+        return $this->cheeseListings;
+    }
+
+    public function addCheeseListing(CheeseListing $cheeseListing): static
+    {
+        if (!$this->cheeseListings->contains($cheeseListing)) {
+            $this->cheeseListings->add($cheeseListing);
+            $cheeseListing->setOwner($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCheeseListing(CheeseListing $cheeseListing): static
+    {
+        if ($this->cheeseListings->removeElement($cheeseListing)) {
+            // set the owning side to null (unless already changed)
+            if ($cheeseListing->getOwner() === $this) {
+                $cheeseListing->setOwner(null);
+            }
+        }
+
+        return $this;
+    }
+}
